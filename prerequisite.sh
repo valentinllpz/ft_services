501bd38f65c3e209c20848eb44b4a217730cfcae
valentinllpz <lugandlopez.valentin@gmail.com>
2021-01-12 18:32:26 +0100

    nginx https ok, do reverse proxy

---
+++ prerequisite.sh
@@ -0,0 +1,10 @@
+#INSTALL KUBECTL
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+#INSTALL MINIKUBE
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
