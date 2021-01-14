179909c2a4b5669959c878fe42d10d093c880a5e
valentinllpz <lugandlopez.valentin@gmail.com>
2021-01-14 15:04:00 +0100

    nginx + phpma ok

--- prerequisite.sh
+++ prerequisite.sh
@@ -8,3 +8,6 @@ sudo apt-get install -y kubectl
 #INSTALL MINIKUBE
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
+
+#TURN NGINX OFF
+sudo service nginx stop
