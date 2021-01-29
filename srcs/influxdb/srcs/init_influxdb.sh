eb296b3973399246eaec8062583deaaa75c2067e
valentinllpz <lugandlopez.valentin@gmail.com>
2021-01-29 16:26:43 +0100

    c'est confini :(

--- srcs/influxdb/srcs/init_influxdb.sh
+++ srcs/influxdb/srcs/init_influxdb.sh
@@ -1,3 +1,5 @@
-service influxdb start
+rc-service influxdb start
+echo "CREATE DATABASE metrics" | influx
+echo "CREATE USER 'metrics_vlugand' WITH PASSWORD 'metrics@ft_53rv1c35' WITH ALL PRIVILEGES" | influx
 telegraf
 sleep infinity
