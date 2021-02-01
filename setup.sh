a06c4132662fcc91fe1cb6533c7f2519fea8b739
valentinllpz <lugandlopez.valentin@gmail.com>
2021-02-01 16:29:31 +0100

    telegraf OK - plus que FTPS

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 #minikube start 
 
 ### METALLB INSTALLATION ###
-#kubectl get configmap kube-proxy -n kube-system -o yaml | \
-#sed -e "s/strictARP: false/strictARP: true/" | \
-#kubectl diff -f - -n kube-system
-#kubectl get configmap kube-proxy -n kube-system -o yaml | \
-#sed -e "s/strictARP: false/strictARP: true/" | \
-#kubectl apply -f - -n kube-system
-#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 ### PREPARING DOCKER IMAGES ###
 eval $(minikube docker-env)
@@ -26,6 +26,7 @@ kubectl apply -f srcs/metallb.yaml
 kubectl apply -f srcs/nginx.yaml
 kubectl apply -f srcs/mysql-pv.yaml
 kubectl apply -f srcs/mysql.yaml
+#kubectl apply -f srcs/telegraf.yaml
 kubectl apply -f srcs/influxdb-pv.yaml
 kubectl apply -f srcs/influxdb.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
--- srcs/influxdb/srcs/init_influxdb.sh
+++ srcs/influxdb/srcs/init_influxdb.sh
@@ -1,5 +1,5 @@
 rc-service influxdb start
 echo "CREATE DATABASE metrics" | influx
 echo "CREATE USER 'metrics_vlugand-' WITH PASSWORD 'metrics@ft_53rv1c35' WITH ALL PRIVILEGES" | influx
-telegraf
+rc-service telegraf start
 sleep infinity
