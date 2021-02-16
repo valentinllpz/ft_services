a08c986f89918dbf06d96daa05b1706fded5032f
valentinllpz <lugandlopez.valentin@gmail.com>
2021-02-16 17:18:33 +0100

    fixed issues due to restructuration - liveness remaining still

--- srcs/mysql/srcs/conf.sql
+++ srcs/mysql/srcs/conf.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE wordpress;
 CREATE USER 'wp_vlugand-'@'%' IDENTIFIED BY 'd3BAZnRfNTNydjFjMzUK';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_vlugand-'@'%';
 CREATE USER 'pma_vlugand-'@'%' IDENTIFIED BY 'cG1hQGZ0XzUzcnYxYzM1Cg==';
