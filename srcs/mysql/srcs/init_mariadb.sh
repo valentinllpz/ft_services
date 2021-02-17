83c85e3f28f50f2fd02184dac816c0b0338228bf
valentinllpz <lugandlopez.valentin@gmail.com>
2021-02-17 13:03:59 +0100

    liveness

--- srcs/mysql/srcs/init_mariadb.sh
+++ srcs/mysql/srcs/init_mariadb.sh
@@ -6,5 +6,5 @@ sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-ser
 sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 rc-service mariadb restart
 mysql < /tmp/wordpress.sql
-mysql < /tmp/conf.sql
+mysql wordpress < /tmp/conf.sql
 sleep infinity
